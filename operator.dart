025e37f3dff35d255b17ae61a9d3a67111da4010
Manish <ish.man.7277@gmail.com>
2024-11-04 15:38:07 +0530

    04/11/24

---
+++ operator.dart
@@ -0,0 +1,71 @@
+void main() {
+// // addition operator
+//   int a = 2;
+//   int b = 4;
+//   print("The sum is : ${a + b}");
+// // subtraction operator.
+//   print("$a -$b=${a - b}");
+// // multipication operator.
+//   print(a * b);
+// // division operator.
+//   int c = 10;
+//   int d = 3;
+//   print(c / d); // anwser can be taken in float or double.
+// // only find quatient values it need "~/".
+//   print(c ~/ d);
+// // for remainder part it need %.
+//   print(c % d);
+// // pre and post increment operator.
+//   int e = 2;
+// // pre increment.
+//   print(++e);
+// // post increment.
+//   print(e++);
+// // print e.
+// // in post increment e will be 3 but if again print hte value then it will be 4.
+//   print(e);
+// // post decrement. same for post decrement.
+//   print(e--);
+// // print e.
+//   print(e);
+// // post decrement.
+//   print(--e);
+// assignmnet operator.
+//compound operator (+=,-= etc).
+//   int f = 3;
+//   print(f = f + f + 1);
+//   print(f += 2);
+// // same for all operator.
+// // conditional operator.
+//   var a = 2;
+//   var b = 3;
+//   var c = 1;
+//   // var d = a == (b - c);
+//   var d = a <= (b - c);
+//   print(d);
+// // assigning the values of any no.
+//   var m;
+//   m ??= a - b;
+//   print(m);
+// // logical operator.
+//   var g = 2;
+//   var h = 3;
+//   var i = 1;
+//   var j = 2;
+//   var k = 3;
+//   var l = 4;
+//   print(g == h && i == j || k == l);
+//   var q;
+//   print(q = a == b);
+// // print about string and int.
+//   String s = "hello";
+//   int t = 1;
+//   print(s is String);
+//   print(t is! int);
+// as use of AS.
+
+// ternary operator.
+  int age = 23;
+  var c = (age > 18) ? "eligilbe" : "not eligible";
+  print(c);
+}
